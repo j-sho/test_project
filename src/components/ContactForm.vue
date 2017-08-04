c8de1a51cdb0da9bfa6fac05ac120ca4bf4baaf2
Julia <shor.jul@gmail.com>
2017-08-04 10:09:15 +0300

    Update ContactForm.vue

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -6,53 +6,53 @@
     </div>
     <hr>
     <form>
-      <FormBlock v-bind:class='{ "error-style": !user_email}'>
+      <FormBlock v-bind:class='{ "error-style": user_email_active && !user_email}'>
         <div slot="block-body">
-          <div class="email_input">
+          <div class="row">
             <label for="user_email"><h4>Адрес электронной почты<span class="form-required"> *</span></h4></label><br>
             <div class="col-md-4">
               <span class="md-form">
-                <input type="email" id="user_email" name="user_email" class="form-control" v-model="user_email">
+                <input type="email" id="user_email" name="user_email" class="form-control" v-model="user_email" @click="user_email_active = true">
               </span>
             </div>
           </div>
           <br>
-          <div class="alert-danger error-message" v-show='!user_email'>
+          <div class="alert-danger error-message" v-show='user_email_active && !user_email'>
             <span class="glyphicon glyphicon-warning-sign error-sign"></span>
               Поле обязательно для заполнения
           </div>
-          <div class="alert-danger error-message" v-show='user_email && !isEmailValid'>
+          <div class="alert-danger error-message" v-show='user_email_active && user_email && !isEmailValid'>
             <span class="glyphicon glyphicon-warning-sign error-sign"></span>
              Данные указаны некорректно
           </div>
         </div>
       </FormBlock>
-      <FormBlock v-bind:class='{ "error-style": !user_contact_mobile}'>
+      <FormBlock v-bind:class='{ "error-style": user_mobile_active && !user_contact_mobile}'>
         <div slot="block-body">
-          <div class="user_contact_mobile">
+          <div class="row">
             <label for="uuser_contact_mobile"><h4>Телефон<span class="form-required"> *</span></h4></label><br>
             <div class="col-md-4">
               <span class="md-form">
-                <input type="tel" id="user_contact_mobile" name="user_contact_mobile" class="form-control" v-model="user_contact_mobile">
+                <input type="tel" id="user_contact_mobile" name="user_contact_mobile" class="form-control" v-model="user_contact_mobile" @click="user_mobile_active = true">
               </span>
             </div>
           </div>
           <br>
-          <div class="alert-danger error-message" v-show='!user_contact_mobile'>
+          <div class="alert-danger error-message" v-show='user_mobile_active && !user_contact_mobile'>
             <span class="glyphicon glyphicon-warning-sign error-sign"></span>
               Поле обязательно для заполнения
           </div>
-          <div class="alert-danger error-message" v-show='user_contact_mobile && !isNumberValid'>
+          <div class="alert-danger error-message" v-show='user_mobile_active && user_contact_mobile && !isNumberValid'>
             <span class="glyphicon glyphicon-warning-sign error-sign"></span>
              Данные указаны некорректно
           </div>
         </div>
       </FormBlock>
+      </form>
       <div class="block_item">
         <button type="submit" class="btn btn-primary center-block">Далее</button>
       </div>
       <p><pre>data: {{$data}}</pre></p>
-    </form>
   </div>
 </template>
 
@@ -64,7 +64,9 @@ export default {
   data() {
     return {
       user_email: '',
-      user_contact_mobile: ''
+      user_contact_mobile: '',
+      user_email_active: false,
+      user_mobile_active: false,
     }
   },
   components: {
@@ -78,6 +80,9 @@ export default {
     },
     isNumberValid() {
       return /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(this.user_contact_mobile);
+    },
+    isErrors() {
+      return (!this.user_email || !this.user_contact_mobile || !this.isEmailValid || !this.isNumberValid)
     }
     }
 }
@@ -93,6 +98,59 @@ export default {
   text-align: left;
   padding-top: 20px;
 }
+
+h1 {
+  font-size: 2em;
+  font-weight: bold;
+}
+
+h2 {
+    line-height: 1.618em;
+    font-size: 1.714em;
+    font-weight: bold;
+}
+
+h4 {
+  font-size: 1.1em;
+  font-weight: bold;
+}
+
+button {
+  margin: 0 auto;
+}
+
+.error-style {
+  background-color: #FAA;
+}
+
+.error-message {
+  background-color: #ffd4d4;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 5px 15px;
+  margin: 0;
+}
+
+.error-sign {
+  color: red;
+}
+
+.form-required {
+  color: red;
+}
+
+
+.form-check-label {
+  font-weight: normal;
+  font-size: 1em;
+}
+
+.button-style {
+  min-width: 100px;
+  font-size: 1.3em;
+  font-weight: bold;
+}
+
 
 
 </style>
