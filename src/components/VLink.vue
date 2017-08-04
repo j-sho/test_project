401a1c4077edf1792b188e34d52f4d516bf89bf7
Julia <shor.jul@gmail.com>
2017-08-04 10:29:01 +0300

    Add files via upload

---
+++ src/components/VLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <a
+    v-bind:href="href"
+    v-bind:class="{ active: isActive }"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+  import routes from '../router'
+  export default {
+    props: {
+      href: String,
+      required: true
+    },
+    computed: {
+      isActive () {
+        return this.href === window.location.pathname
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .active {
+    color: cornflowerblue;
+  }
+</style>
