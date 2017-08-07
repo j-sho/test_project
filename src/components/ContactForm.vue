e5a09220908d0e71bf4a8700fb28405f2fb111f9
Julia <shor.jul@gmail.com>
2017-08-07 11:55:22 +0300

    Update ContactForm.vue

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -11,8 +11,19 @@
           <div class="row">
             <label for="user_email"><h4>Адрес электронной почты<span class="form-required"> *</span></h4></label><br>
             <div class="col-md-4">
-              <span class="md-form">
-                <input type="email" id="user_email" name="user_email" class="form-control" v-model="user_email" @click="user_email_active = true">
+              <span class="md-form" @click="user_email_active = true">
+                <masked-input
+                  type="email"
+                  name="email"
+                  class="form-control"
+                  id="user_email"
+                  v-model="user_email"
+                  :mask="mask"
+                  :guide="true"
+                  :keepCharPositions="true"
+                  :showMask="true"
+                  placeholderChar="_">
+                </masked-input>
               </span>
             </div>
           </div>
@@ -27,31 +38,47 @@
           </div>
         </div>
       </FormBlock>
-      <FormBlock v-bind:class='{ "error-style": user_mobile_active && !user_contact_mobile}'>
+      <br>
+      <FormBlock v-bind:class='{ "error-style": user_mobile_active && isNumberNotvalid }'>
         <div slot="block-body">
           <div class="row">
-            <label for="uuser_contact_mobile"><h4>Телефон<span class="form-required"> *</span></h4></label><br>
+            <label for="user_contact_mobile"><h4>Телефон<span class="form-required"> *</span></h4></label><br>
             <div class="col-md-4">
-              <span class="md-form">
-                <input type="tel" id="user_contact_mobile" name="user_contact_mobile" class="form-control" v-model="user_contact_mobile" @click="user_mobile_active = true">
+              <span class="md-form" @click="user_mobile_active = true">
+                <masked-input
+                  type="text"
+                  name="phone"
+                  class="form-control"
+                  id="user_contact_mobile"
+                  v-model="user_contact_mobile"
+                  :mask="['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]"
+                  :guide="true"
+                  :keepCharPositions="false"
+                  :showMask="true"
+                  placeholderChar="_">
+                </masked-input>
               </span>
             </div>
           </div>
           <br>
-          <div class="alert-danger error-message" v-show='user_mobile_active && !user_contact_mobile'>
-            <span class="glyphicon glyphicon-warning-sign error-sign"></span>
-              Поле обязательно для заполнения
-          </div>
-          <div class="alert-danger error-message" v-show='user_mobile_active && user_contact_mobile && !isNumberValid'>
+          <div class="alert-danger error-message" v-show='user_mobile_active && isNumberNotvalid'>
             <span class="glyphicon glyphicon-warning-sign error-sign"></span>
              Данные указаны некорректно
           </div>
         </div>
       </FormBlock>
       </form>
-      <div class="block_item">
-        <button type="submit" class="btn btn-primary center-block">Далее</button>
-      </div>
+      <br>
+      <div class="button_block">
+        <div class="block_item">
+          <router-link to="/form">
+            <button type="cancel" class="btn btn-primary center-block button-style">Назад</button>
+          </router-link>
+        </div>
+        <div class="block_item">
+          <button type="submit" class="btn btn-primary center-block button-style" v-on:click="submitForm">Далее</button>
+        </div>
+      </div><br><br>
       <p><pre>data: {{$data}}</pre></p>
   </div>
 </template>
@@ -59,30 +86,42 @@
 <script>
 import Vue from 'vue'
 import FormBlock from './FormBlock'
+import MaskedInput from 'vue-text-mask'
+import emailMask from 'text-mask-addons/dist/emailMask'
 
 export default {
+  name: 'name',
   data() {
     return {
-      user_email: '',
+      user_email: 'aaaaaa@aaaaaa.aa',
       user_contact_mobile: '',
       user_email_active: false,
       user_mobile_active: false,
+      mask: emailMask,
     }
   },
   components: {
-    FormBlock
+    FormBlock,
+    MaskedInput,
+    emailMask
   },
   methods: {
+    submitForm: function(){
+      if (!this.isErrors) {
+        console.log("HELLO");
+        this.$router.push('/submission_form');
+      } else {console.log("CHAO");}
+    }
   },
   computed: {
     isEmailValid() {
       return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.user_email);
     },
-    isNumberValid() {
-      return /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(this.user_contact_mobile);
+    isNumberNotvalid() {
+      return /_/.test(this.user_contact_mobile);
     },
     isErrors() {
-      return (!this.user_email || !this.user_contact_mobile || !this.isEmailValid || !this.isNumberValid)
+      return (!this.user_email || !this.user_contact_mobile || !this.isEmailValid || this.isNumberNotvalid)
     }
     }
 }
@@ -149,6 +188,12 @@ button {
   min-width: 100px;
   font-size: 1.3em;
   font-weight: bold;
+}
+
+.button_block {
+  display: flex;
+  justify-content: space-around;
+  padding: 0 200px;
 }
 
 
